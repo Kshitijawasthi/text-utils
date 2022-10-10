ffcdb53c5c8985b8bb7572bb551c2bd5784ebdbc
Kshitijawasthi <113694225+Kshitijawasthi@users.noreply.github.com>
2022-10-10 13:24:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import React, { useState } from 'react'
+import Alert from "./Components/Alert";
 
-function App() {
+export default function App() {
+ const [mode, setMode] = useState('dark');
+ const [alert, setAlert] = useState(null);
+
+ const showAlert=(message,type)=>{
+     setAlert({
+        msg: message,
+        type: type 
+     })
+     setTimeout(() => {
+      setAlert(null);
+     }, 1500);
+ }
+
+   const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled","success");
+      // document.title='Textutils - Dark Mode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
+      // document.title='Textutils - Light Mode';
+    }
+   }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     
+      <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">        
+          <TextForm showAlert={showAlert} heading="Textutils-Word Counter, Character Counter, Remove extra space" mode={mode}/>
+          <About mode={mode} />
+        </div>   
+    </>
   );
 }
 
-export default App;
+// export default App;
